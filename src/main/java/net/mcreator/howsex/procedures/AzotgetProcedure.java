b78dedaba9f15b9f110af0cb8b9b59eea7673e81
Mrokac <Admin@192.168.1.31>
2023-01-29 15:59:37 +0100

    naprawione wszystko

--- src/main/java/net/mcreator/howsex/procedures/AzotgetProcedure.java
+++ src/main/java/net/mcreator/howsex/procedures/AzotgetProcedure.java
@@ -23,21 +23,12 @@ public class AzotgetProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		if (new Object() {
-			public int getAmount(int sltid) {
-				if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
-						&& _current.get() instanceof Map _slots) {
-					ItemStack stack = ((Slot) _slots.get(sltid)).getItem();
-					if (stack != null)
-						return stack.getCount();
-				}
-				return 0;
-			}
-		}.getAmount(1) == 0 && (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
-				&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY).getItem() == Items.DIAMOND) {
+		if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get(1)).getItem()
+				: ItemStack.EMPTY).getItem() == Items.DIAMOND) {
 			if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
 					&& _current.get() instanceof Map _slots) {
-				((Slot) _slots.get(0)).remove(1);
+				((Slot) _slots.get(1)).remove(1);
 				_player.containerMenu.broadcastChanges();
 			}
 			if (world instanceof Level _level) {
@@ -63,7 +54,7 @@ public class AzotgetProcedure {
 						&& _current.get() instanceof Map _slots) {
 					ItemStack _setstack = new ItemStack(HowsexModItems.AZOT.get());
 					_setstack.setCount(1);
-					((Slot) _slots.get(1)).set(_setstack);
+					((Slot) _slots.get(2)).set(_setstack);
 					_player.containerMenu.broadcastChanges();
 				}
 			});
